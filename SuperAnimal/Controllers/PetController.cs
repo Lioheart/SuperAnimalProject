976c4b415c77ddafc5856d1effd3e74ac79f1be7
Marcin <ziewa91@wp.pl>
2020-03-08 22:09:16 +0100

    WIP Profile layout

---
+++ SuperAnimal/Controllers/PetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperAnimal.Controllers
+{
+    public class PetController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+        // GET: /Pet/AddPet
+        [HttpGet]
+        public IActionResult AddPet() => View();
+        
+    }
+}
---
+++ SuperAnimal/ViewModels/Profile/ProfileIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SuperAnimal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAnimal.ViewModels.Profile
+{
+    public class ProfileIndexViewModel
+    {
+        public IEnumerable<Pet> Pets;
+    }
+}
