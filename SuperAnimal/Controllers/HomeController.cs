ebc58378dcfe66376e2741dbef781034aa379113
Marcin <ziewa91@wp.pl>
2020-05-28 20:02:32 +0200

    api consumption and AJAX by Axios

--- SuperAnimal/Controllers/HomeController.cs
+++ SuperAnimal/Controllers/HomeController.cs
@@ -4,11 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
 using SuperAnimal.Data;
 using SuperAnimal.Models;
+using SuperAnimal.ViewModels.Home;
 
 namespace SuperAnimal.Controllers
 {
@@ -39,5 +43,36 @@ namespace SuperAnimal.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+
+        public IActionResult Covid()
+        {
+            var client = new RestClient("https://covid-19-data.p.rapidapi.com/totals?format=json");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "fe7e68095dmsh3ab1bccf87f3dcdp1d3d30jsne245d22c32b8");
+            IRestResponse response = client.Execute(request);
+
+            var result =  JsonConvert.DeserializeObject<List<CovidViewModel>>(response.Content);
+
+            return View(result.First());
+        }
+
+        public IActionResult CovidJSON()
+        {
+            var client = new RestClient("https://covid-19-data.p.rapidapi.com/totals?format=json");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "fe7e68095dmsh3ab1bccf87f3dcdp1d3d30jsne245d22c32b8");
+            IRestResponse response = client.Execute(request);
+
+            //var json = JsonConvert.DeserializeObject(response.Content);
+
+            //var xd = json.
+
+            return Ok(response.Content);
+
+            
+
+        }
     }
 }
--- SuperAnimal/wwwroot/js/site.js
+++ SuperAnimal/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function getResponseCovidJSON() {
+
+    const confirmed = $("#h-confirmed");
+    const recovered = $("#h-recovered");
+    const critical = $("#h-critical");
+    const deaths = $("#h-deaths")
+    const lastChange = $("#h-last-change");
+    const lastUpdate = $("#h-last-update");
+
+
+    axios.get('/home/covidjson')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+
+            const data = response.data[0];
+
+            confirmed.text("Confirmed: " + data.confirmed);
+            recovered.text("Recovered: " + data.recovered);
+            critical.text("Critical: " + data.critical);
+            deaths.text("Deaths: "+data.deaths);
+            lastChange.text("Last Change: " +data.lastChange);
+            lastUpdate.text("Last Update: "+ data.lastUpdate);
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+
+}
